60d12783a408b7460115522253baffa07c0e69a3
Kaspars Zuks <kaspars.zuks@videinfra.com>
2026-01-15 12:16:25 +0200

    unSignal fix for computed properties

--- src/signal.ts
+++ src/signal.ts
@@ -1,4 +1,4 @@
-import { signal, isSignal } from 'alien-signals';
+import { signal, isSignal, isComputed } from 'alien-signals';
 
 export type SignalType<T> = ReturnType<typeof signal<T>>;
 
@@ -9,7 +9,7 @@ export type SignalType<T> = ReturnType<typeof signal<T>>;
  * @returns The unsignalified value
  */
 export function unSignal<T> (value: SignalType<T>|T):typeof value extends SignalType<T> ? T : T {
-    if (value instanceof signal && isSignal(value)) {
+    if (value instanceof signal && (isSignal(value) || isComputed(value))) {
         return value() as T;
     } else {
         // Using `as` is dirty, should fix this
